3a129b1625c42efe52d4b535a13ad8647b5542b2
Ashrit Kommireddy <ashritkommireddy@Ashrits-MacBook-Air.local>
2026-04-27 18:07:14 -0400

    Revert to CORSMiddleware with credentials False

--- listingai/backend/api.py
+++ listingai/backend/api.py
@@ -33,25 +33,15 @@ class GenerateResponse(BaseModel):
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
 
-@app.middleware("http")
-async def add_cors_headers(request, call_next):
-    response = await call_next(request)
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "*"
-    response.headers["Access-Control-Allow-Headers"] = "*"
-    return response
-
-
-@app.options("/generate")
-async def options_generate():
-    from fastapi.responses import Response
-
-    response = Response()
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "POST, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "*"
-    return response
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 PROMPT_TEMPLATE = """You are an expert real estate copywriter. Generate 3 different 
